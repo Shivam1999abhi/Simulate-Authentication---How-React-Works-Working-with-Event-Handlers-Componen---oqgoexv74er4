425086f755730661788777f0828dab96ed31052a
Shivam1999abhi <116838168+Shivam1999abhi@users.noreply.github.com>
2023-02-17 22:52:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import User from "../models/user";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const [signupForm, setSignUpForm] = useState({
+    signupEmail: "",
+    signupPassword: "",
+    signupConfirmPassword: "",
+    signupName: ""
+  });
+
+  const [loginForm, setLoginForm] = useState({
+    loginEmail: "",
+    loginPassword: ""
+  });
+
+  const onChangeSignupForm = (event) => {
+    setSignUpForm({
+      ...signupForm,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const onChangeLoginForm = (event) => {
+    setLoginForm({
+      ...loginForm,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const getUser = (email) => {
+    return users.find((user) => user.email === email) || null;
+  };
+
+  const registerUser = async (email, password, confirmPassword, name) => {
+    if (password !== confirmPassword || getUser(email)) {
+      return false;
+    }
+  };
   return (
     <div id="main">
       <table id="all-users">
-      <tbody>
+        <tbody>
           <tr>
             <th>Name</th>
             <th>Email</th>
@@ -23,16 +61,36 @@ const App = () => {
       <div>
         <form className="signup-form">
           <label htmlFor="name">Name</label>
-          <input type="text" name="signupName" id="signupName" />
+          <input
+            type="text"
+            name="signupName"
+            id="signupName"
+            onChange={onChangeSignupForm}
+            value={signupForm.signupName}
+          />
           <label htmlFor="email">Email</label>
-          <input type="email" name="signupEmail" id="signupEmail" />
+          <input
+            type="email"
+            name="signupEmail"
+            id="signupEmail"
+            onChange={onChangeSignupForm}
+            value={signupForm.signupEmail}
+          />
           <label htmlFor="password">Password</label>
-          <input type="password" name="signupPassword" id="signupPassword" />
+          <input
+            type="password"
+            name="signupPassword"
+            id="signupPassword"
+            onChange={onChangeSignupForm}
+            value={signupForm.signupPassword}
+          />
           <label htmlFor="confirmPassword">Confirm Password</label>
           <input
             type="password"
             name="signupConfirmPassword"
             id="signupConfirmPassword"
+            onChange={onChangeSignupForm}
+            value={signupForm.signupConfirmPassword}
           />
         </form>
         <button id="signup-button">Signup</button>
